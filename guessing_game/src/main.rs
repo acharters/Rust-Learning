4650c7648d6228ec26575300a8e0a91f30978d4d
Drew <drew.charters@outlook.com>
2018-10-06 17:36:58 -0700

    program loops until user guesses correctly

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,18 +8,22 @@ fn main() {
     //generate secret number
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number); //for debugging
-    //get input from the user
-    println!("Guess a number: ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Couldn't read line!");
-    let guess: i32 = guess.trim().parse()
-        .expect("This is not a number!");
-    println!("your number: {}", guess);
-    //check to see if the user's guess is too big or too small
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Guess is too small."),
-        Ordering::Greater => println!("Guess is too big."),
-        Ordering::Equal => println!("This is the correct number!")
+    //loop until user guesses correctly
+    loop {
+        println!("Guess a number: ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Couldn't read line!");
+        let guess: i32 = guess.trim().parse()
+            .expect("This is not a number!");
+        println!("your number: {}", guess);
+        //check to see if the user's guess is too big or too small
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Guess is too small."),
+            Ordering::Greater => println!("Guess is too big."),
+            Ordering::Equal => { println!("This is the correct number!");
+                break;
+            }
+        }
     }
 }
