5064ccd8ebe885544dd18a3f9447ff107385e3cf
Drew <drew.charters@outlook.com>
2018-12-10 22:25:46 -0800

    example of User struct tested

--- structTest/src/main.rs
+++ structTest/src/main.rs
@@ -1,4 +1,39 @@
 
+fn main() {
+    let user1 = User {
+        username: String::from("myUsername"),
+        signin_count: 0,
+        active: true,
+        email: String::from("drew@email.com"),
+        int1: 4,
+        int2: 7
+    };
+    //let s1 = user.username;
+    println!("Username: {}", user1.username);
+    println!("Debug test: {:#?}", user1);
+    println!("Testing method: {}", user1.multiply());
+}
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    signin_count: i32,
+    active: bool,
+    int1: i32,
+    int2: i32
+}
+
+impl User {
+    fn multiply(&self) -> i32 {
+        self.int1 * self.int2
+    }
+}
+
+
+
+
+
 //testing mutability of String
 
 /*
