0b2e41138737a270112425a049cf588a61127e5e
Drew <drew.charters@outlook.com>
2018-12-10 21:54:09 -0800

    ownership testing

---
+++ structTest/src/main.rs
@@ -0,0 +1,24 @@
+
+//testing mutability of String
+
+/*
+fn main() {
+    let mut string = String::from("test");
+    println!("My string: {}", string);
+    modify_str(&mut string);
+    println!("Last string check: {}", string);
+    println!("Length: {}", get_len(&string));
+    println!("One last string check: {}", string);
+}
+
+fn modify_str(s: &mut String) {
+    println!("My string inside the function: {}", s);
+    s.push_str(" more input");
+    println!("Another check: {}", s);
+}
+
+fn get_len(s: &String) -> usize {
+    s.len()
+}
+*/
+
