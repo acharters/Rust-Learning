e237aa5bba45a7c3b292c765fa3ebf1a1a1ec401
Drew <drew.charters@outlook.com>
2018-12-12 21:45:50 -0800

    added if let statement

--- enum_example/src/main.rs
+++ enum_example/src/main.rs
@@ -18,18 +18,27 @@ fn main() {
     let e1 = MyEnum::One;
     let t = (9, String::from("Tuple str"));
     let e2 = MyEnum::Two(t);
-    let e3 = MyEnum::Three(x);
+    let mut e3 = MyEnum::Three(x);
     //println!("{} | {}", e2.Two.2, e3.Three.name); code doen't work
     let tuple_str = match e2 {
         MyEnum::One => String::from("ERROR"),
         MyEnum::Two((_, y)) => y,
         MyEnum::Three(_) => String::from("ERROR")
     };
-    let username_str = match e3 {
+    let username_str = match &mut e3 {
         MyEnum::One => String::from("ERROR"),
         MyEnum::Two(_) => String::from("ERROR"),
-        MyEnum::Three(x) => x.name
+        MyEnum::Three(n) => {
+            n.name.push_str(" appended");
+            n.name.clone()
+        }
     };
-    //println!("{} | {}", tuple_str, username_str);
-    
+    println!("{} | {}", tuple_str, username_str);
+    //println!("{}", x.name); doesn't print because x was moved into e3
+    if let MyEnum::Three(m) = e3 {
+        println!("If condition: {}", m.name);
+    } else {
+        //never reached because e3 is a Three variant
+        println!("Else condition");
+    }
 }
